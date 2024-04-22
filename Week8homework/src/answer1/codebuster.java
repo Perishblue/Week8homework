bda66c08ae4fecf42e7b05c9a15a22472cfaa3ec
Perishblue <visnudasdarshan@gmail.com>
2024-04-22 17:33:37 +0100

    my first push

---
+++ Week8homework/src/answer1/codebuster.java
@@ -0,0 +1,17 @@
+package answer1;
+
+public class codebuster {
+      /*Write a Java program to create a class called codebuster with a method called
+            groupName(). Create a subclass called codebusterGroups() that overrides the
+    groupName() method.*/
+
+    public static void m1()
+    {System.out.println("groupname");}
+
+    public static void main(String[] args) {
+        codebuster name = new codebuster();
+        codebuster.m1();
+
+    }
+}
+
